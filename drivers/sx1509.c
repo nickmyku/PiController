b61c9d3b21a0b784ac348d9e5b7ce890da9be27d
nickmyk <nanosecrepair@gmail.com>
2014-04-23 17:47:15 +0000

    added drivers for sx1509 IO expander board, this controls W axis motor, W axis limit switches, and LEDs

---
+++ drivers/sx1509.c
@@ -0,0 +1,630 @@
+//driver for the sx1509 io expander board in the REV 2 3d metal printer prototype
+//Nicholas Mykulowycz
+//Created April 20, 2014
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+//global varible declarations
+const int motorPwmPin = 3;	//pin (on Data Register A) that the pwm signal of the dc motor driver is connected to
+const int w_lim_0 = 0;		//pin (on Data Register B) where w limit switch 0 is connected	
+const int w_lim_1 = 1;		//pin (on Data Regsiter B) where w limit switch 1 is connected
+const int timeout = 200000;	//timeout value for w axis "move to limit" function
+const int delayMS = 100;	//delay between limit switch checks on w axis "move to limit" function
+
+
+
+//sx1509 internal register addresses
+const int SXADDR =	0x3e;
+const int REGRESET = 	0x7d;
+const int REGPOLARITYA =	0x0d;
+const int REGPOLARITYB =	0x0c;
+const int REGPULLUPA =	0x07;
+const int REGPULLUPB =	0x06;
+const int REGDIRA =	0x0f;
+const int REGDIRB =	0x0e;
+const int REGINPUTDISABLEA =	0x01;
+const int REGINPUTDISABLEB =	0x00;
+const int REGOPENDRAINA = 	0x0b;
+const int REGOPENDRAINB = 	0x0a;
+const int REGDATAA =	0x11;
+const int REGDATAB =	0x10;
+const int REGMISC =	0x1f;
+const int REGCLOCK =	0x1e;
+const int REGLEDDRIVERENABLEA =	0x21;
+const int REGLEDDRIVERENABLEB = 0x20;
+int REGION[16] =	{0x2a,0x2d,0x30,0x33,0x36,0x3b,0x40,0x45,0x4a,0x4d,0x50,0x53,0x56,0x5b,0x60,0x65};
+int REGTON[16] =	{0x29,0x2c,0x2f,0x32,0x35,0x3a,0x3f,0x44,0x49,0x4c,0x4f,0x52,0x55,0x5a,0x5f,0x64};
+int REGOFF[16] = 	{0x2b,0x2e,0x31,0x34,0x37,0x3c,0x41,0x46,0x4b,0x4e,0x51,0x54,0x57,0x5c,0x61,0x66};
+int REGTRISE[16] =	{   0,   0,   0,   0,0x38,0x3d,0x42,0x47,   0,   0,   0,   0,0x58,0x5d,0x62,0x67};
+int REGTFALL[16] =	{   0,   0,   0,   0,0x39,0x3e,0x43,0x48,   0,   0,   0,   0,0x59,0x5e,0x63,0x68};
+
+//prototpye function declarations
+void writeReg(int device, int memRegister, int data);
+int readReg(int device, int memRegister);
+int intToBits(bool * array, int number);
+int bitsToInt(bool array[8]);
+int scaleIntensity(int num);
+int initializeDevice();
+int LEDBreath(int device, int pin, int onIntensity, int offIntensity);
+int LEDColor(int device, int LEDNum, int red, int green, int blue);
+int motorMove(int device, bool dir, int speed);
+int motorStop(int device, bool brake);
+int moveToLimit(int device, bool dir, int speed, bool brake);
+bool readWLimit(int device, int switchNum);
+
+void rave(int device, int cycles, int delayMS);
+
+int main (void)
+{
+	//create identifier for sx1509 io expander
+	int sx;
+	
+	sx = initializeDevice();
+	
+	/*
+	//establish i2c connection to sx1509
+	if((sx = wiringPiI2CSetup(SXADDR)) == -1)
+	{
+		fprintf(stderr, "sx: unable to initialize I2C %s, activating self destruct sequence\n", strerror (errno));
+		printf("error: function \"selfDestruct()\" not defined in scope of program\n");
+		printf("alright fine you win this time\n");
+		return -1;
+	}
+	*/
+	
+	/*
+	//calls function which sets input and output pin parameters
+	if(initPinMode(sx) != 0)
+	{
+		printf("error setting up pin modes\n");
+		
+		return -1;
+	}
+	*/
+	
+	//disabled outputs, led driver on
+	//writeReg (sx, REGDATAA, 0b00000000);
+	//writeReg (sx, REGDATAB, 0b00000000);
+	
+	//bool localbits[8];
+	
+	//LEDColor(sx, 0, 1,254,1);
+	//int var =121;
+	//printf("%d\n",var);
+	//intToBits(localbits,var);
+	//var = bitsToInt(localbits);
+	//printf("%d\n",var);
+	
+	//motorMove(sx, false, 200);
+	//delay(1000);
+	//motorStop(sx, false);
+	
+	//rave(sx, 1000, 76);
+	
+	LEDBreath(sx, 4, 255, 50);
+	LEDBreath(sx, 12, 255, 50);
+	
+	moveToLimit(sx, false, 200, true);	
+	
+	printf("W limit 0 depressed - %d\n", readWLimit(sx, 0));
+	printf("W limit 1 depressed - %d\n", readWLimit(sx, 1));
+	
+	printf("program complete\n");
+
+	return 0;
+}
+
+int initializeDevice ()
+{
+	int device;
+	
+	//intialize I2C connection to sx1509 
+	if((device = wiringPiI2CSetup(SXADDR)) == -1)
+	{
+		fprintf(stderr, "sx: unable to initialize I2C %s, activating self destruct sequence\n", strerror (errno));
+		printf("error: function \"selfDestruct()\" not defined in scope of program\n");
+		printf("alright fine you win this time\n");
+		return -1;
+	}
+	
+	//software reset of device
+	writeReg (device, REGRESET, 0x12);
+	writeReg (device, REGRESET, 0x34);
+	
+	//intialize pin modes on sx1509
+	writeReg (device, REGDIRA, 0b10000000);		//register A pin modes
+	writeReg (device, REGDIRB, 0b10001111);		//register B pin modes
+	
+	//enables oscillator, led drivers dont work when its disabled
+	writeReg (device, REGCLOCK, 0b01000000);
+	
+	//misc register setup, log fade and oscillation divider
+	writeReg (device, REGMISC, 0b11000000);
+	
+	//enables led driver for pins 4, 5, 6 Also pin 3 - motor speed control
+	writeReg (device, REGLEDDRIVERENABLEA, 0b01111000);
+	//enabled LED driver for pins 12, 13, 14
+	writeReg (device, REGLEDDRIVERENABLEB, 0b01110000);
+	
+	//disable input buffer on LED/PWM pins
+	writeReg (device, REGINPUTDISABLEA, 0x01111000);
+	writeReg (device, REGINPUTDISABLEB, 0b01110000);
+	
+	//enable open drain on LED/PWM pins
+	writeReg (device, REGOPENDRAINA, 0b01111000);
+	writeReg (device, REGOPENDRAINB, 0b01110000);
+	
+	//enable pull up resitors on LED/PWM pins
+	writeReg (device, REGPULLUPA, 0b01111000);
+	writeReg (device, REGPULLUPB, 0b01110000);
+	
+	//seed random
+	srand(time(NULL));
+	
+	return device;
+}
+
+//makes LED fade in and out
+int LEDBreath(int device, int pin, int onIntensity, int offIntensity)
+{
+	int tempReg;
+	int tempPin;
+	int reg_data;
+	bool bits[8];
+	
+	if(REGTRISE[pin] == 0)
+	{
+		printf("this pin has been asphyxiated, it cannot breath.\n");
+		return -1;
+	}
+	
+	//make sure intensity vale is within range
+	onIntensity = scaleIntensity(onIntensity);
+	offIntensity = scaleIntensity(offIntensity);
+	
+	
+	//sets led intensity
+	writeReg (device, REGION[pin], onIntensity);
+	//sets on time
+	writeReg (device, REGTON[pin], 0b00000010);
+	//sets off time and intensity
+	writeReg (device, REGOFF[pin], 0b00011111);
+	//sets fade in time
+	writeReg (device, REGTRISE[pin], 0b00001111);
+	//sets fade out time
+	writeReg (device, REGTFALL[pin], 0b00001111);	
+	
+	if(pin <8)
+	{
+		tempReg = REGDATAA;
+		tempPin = pin;
+	}
+	else
+	{
+		tempReg = REGDATAB;
+		tempPin = pin - 8;		//accounts for pin value greater than reg size
+	}
+	
+	
+	//read current values at reg
+	reg_data = readReg (device, tempReg);
+	//convert to bits for manipulation reasons
+	intToBits(bits, reg_data);
+			
+	//set bits associated with led pins low
+	bits[tempPin] = false;
+	//convert back to int
+	reg_data = bitsToInt(bits);
+			
+	//enable PWM output by setting output low
+	writeReg (device, tempReg, reg_data);
+
+	return 0;
+}
+
+//sets led to constant color
+int LEDColor(int device, int LEDNum, int red, int green, int blue)
+{
+	int redPin =	4;
+	int greenPin =	5;
+	int bluePin =	6;
+	
+	bool bits[8];
+	int reg_data;
+			
+	//assigns proper output pins for a given led strip
+	switch(LEDNum)
+	{
+		case 0:
+			redPin =	4;
+			greenPin =	5;
+			bluePin =	6;
+			
+			//read current values ar reg a
+			reg_data = readReg (device, REGDATAA);
+			//convert to bits for manipulation reasons
+			intToBits(bits, reg_data);
+			
+			//set bits associated with led pins low
+			bits[redPin] = false;
+			bits[greenPin] = false;
+			bits[bluePin] = false;
+			//convert back to int
+			reg_data = bitsToInt(bits);
+			
+			//enable PWM output by setting output low
+			writeReg (device, REGDATAA, reg_data);
+			break;
+		case 1:
+			redPin =	12;
+			greenPin =	13;
+			bluePin =	14;
+			
+			//read current values ar reg b
+			reg_data = readReg (device, REGDATAB);
+			//convert to bits for manuipulation reasons
+			intToBits(bits, reg_data);
+			
+			//set bits associated with led pins low
+			bits[redPin-8] = false;
+			bits[greenPin-8] = false;
+			bits[bluePin-8] = false;
+			//convert back to int
+			reg_data = bitsToInt(bits);
+			
+			//enable PWM output by setting output low
+			writeReg (device, REGDATAB, reg_data);
+			break;
+		default:
+			printf("there is no LED %d, asshole\n",LEDNum);
+			return -1;
+	}
+	
+	//make usre red green and blue intensities are in range
+	red = scaleIntensity(red);
+	green = scaleIntensity(green);
+	blue = scaleIntensity(blue);
+	
+	//sets the intensitys of the RGB leds
+	writeReg (device, REGION[redPin], red);
+	writeReg (device, REGION[greenPin], green);
+	writeReg (device, REGION[bluePin], blue);
+	 
+	//sets T_ON to default 0x00 (always on)
+	writeReg (device, REGTON[redPin], 0x00);
+	writeReg (device, REGTON[greenPin], 0x00);
+	writeReg (device, REGTON[bluePin], 0x00);
+	
+	//sets OFF to defualt 0x00 (always on)
+	writeReg (device, REGOFF[redPin], 0x00);
+	writeReg (device, REGOFF[greenPin], 0x00);
+	writeReg (device, REGOFF[bluePin], 0x00);
+	
+	//turn off fading if its enabled for that pin
+	int pins[3] = {redPin,greenPin,bluePin};
+	int i;
+	for(i=0; i<3; i++)
+	{
+		if(REGTRISE[pins[i]] != 0)
+		{
+			writeReg (device, REGTRISE[pins[i]], 0x00);
+		}
+		if(REGTFALL[pins[i]] != 0)
+		{
+			writeReg (device, REGTFALL[pins[i]], 0x00);
+		}
+	}
+	
+	//printf("red %d, green %d, blue %d\n", red, green, blue);
+	
+	return 0;
+}
+
+int scaleIntensity(int num)
+{
+	//negative value limit is 0
+	if(num<0)
+	{
+		return 0;
+	}
+	
+	//positive value limit is 255
+	if(num > 255)
+	{
+		return 255;
+	}	
+	
+	//otherwise return the value which was input	
+	return num;
+}
+
+//function is critially important to proper function of driver
+//any attempt to remove this function is considered a captial offense and is punishable by death
+//- a slow and painful death just to be concise
+void rave(int device, int cycles, int delayMS)
+{
+
+	int r0;
+	int g0;
+	int b0;
+	
+	int r1;
+	int g1;
+	int b1;
+	
+	int i;
+	
+	for(i=0; i<cycles; i++)
+	{
+		r0 = rand()%256;
+		g0 = rand()%256;
+		b0 = rand()%256;
+		
+		LEDColor(device, 0, r0, g0, b0);
+		delay(delayMS/2);
+		
+		r1 = rand()%256;
+		g1 = rand()%256;
+		b1 = rand()%256;
+				
+		LEDColor(device, 1, r1, g1, b1);
+		delay(delayMS/2);
+	}
+		
+}
+
+bool readWLimit(int device, int switchNum)
+{
+	int reg_data_b;
+	bool bits[8];
+	
+	//read current values ar reg b
+	reg_data_b = readReg (device, REGDATAB);
+	intToBits(bits, reg_data_b);
+	
+	switch(switchNum)
+	{
+		case 0:
+			if(bits[0])
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		case 1:
+			if(bits[1])
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		default:
+			printf("no limit switch connected to %d\n",switchNum);
+			return false;
+	}
+}
+
+int motorMove(int device, bool dir, int speed)
+{
+	
+	int reg_data_a;
+	bool bits[8];
+	
+	//make sure speed value is in range (<255)
+	speed = scaleIntensity(speed);
+	//account for inverted PWM driver
+	speed = 255 - speed;
+	
+	//read current values ar reg a
+	reg_data_a = readReg (device, REGDATAA);
+	intToBits(bits, reg_data_a);
+	
+	//positive
+	if(!dir)
+	{	
+		bits[0] = true;		//standby pin
+		bits[1] = true;		//B-IN 1 pin
+		bits[2] = false;	//B-IN 2 pin
+		bits[motorPwmPin] = false;	//PWM pin, must be set low to enable pwm
+	}
+	//negative
+	else
+	{
+		bits[0] = true;	//standby pin
+		bits[1] = false;	//B-IN 1 pin
+		bits[2] = true;		//B-IN 2 pin
+		bits[motorPwmPin] = false;	//PWM pin, must be set low to enable pwm
+	}
+	
+	reg_data_a = bitsToInt(bits);
+	
+	//enable motor and set direction
+	writeReg (device, REGDATAA, reg_data_a);
+		
+	//sets the pwm value
+	writeReg (device, REGION[motorPwmPin], speed);
+	//sets T_ON to default 0x00 (always on)
+	writeReg (device, REGTON[motorPwmPin], 0x00);
+	//sets OFF to defualt 0x00 (always on)
+	writeReg (device, REGOFF[motorPwmPin], 0x00);
+	
+	return 0;
+}
+
+int motorStop(int device, bool brake)
+{
+	int reg_data_a;
+	bool bits[8];
+	
+	//read current values at reg a
+	reg_data_a = readReg (device, REGDATAA);
+	intToBits(bits, reg_data_a);
+	
+	//if brake is true enable brake functionality
+	if(brake)
+	{
+		bits[0] = true;		//standby pin
+		bits[1] = true;		//B-IN 1 pin
+		bits[2] = true;		//B-IN 2 pin
+		bits[motorPwmPin] = false;	//PWM pin, must be set low to enable pwm
+	}
+	//otherwise let motor coast to a stop
+	else
+	{
+		bits[0] = true;		//standby pin
+		bits[1] = false;	//B-IN 1 pin
+		bits[2] = false;	//B-IN 2 pin
+		bits[motorPwmPin] = false;	//PWM pin, must be set low to enable pwm
+	}
+	
+	reg_data_a = bitsToInt(bits);
+	
+	//send data to motor pins
+	writeReg (device, REGDATAA, reg_data_a);
+	
+	//sets the pwm value to off
+	writeReg (device, REGION[motorPwmPin], 0xff);
+	//sets T_ON to default 0x00 (always on)
+	writeReg (device, REGTON[motorPwmPin], 0x00);
+	//sets OFF to defualt 0x00 (always on)
+	writeReg (device, REGOFF[motorPwmPin], 0x00);	
+	
+	
+	return 0;
+}
+
+int moveToLimit(int device, bool dir, int speed, bool brake)
+{
+	bool limit;
+	int time = 0;
+	
+	//check to see if w axis has already reched limit
+	if(dir && readWLimit(device, 1))
+	{
+		printf("already at positive limit\n");
+		return -1;
+	}
+	if(!dir && readWLimit(device, 0))
+	{
+		printf("already at negative limit\n");
+		return -2;
+	}
+	
+	//if limit is not reached begin movement
+	motorMove(device, dir, speed);
+	
+	//loop which continuosly checks limit switches until it detects a change or times out
+	if(dir)
+	{
+		while(!limit && (time<timeout))
+		{
+			//check if limit switch is depressed
+			limit = readWLimit(device, 1);
+			
+			//delay to prevert the i2c bus from being flooded
+			delay(delayMS);
+			time += delayMS;
+		}
+	}
+	else
+	{
+		while(!limit && (time<timeout))
+		{
+			//check if limit switch is depressed
+			limit = readWLimit(device, 0);
+			
+			//delay to prevent i2c bus from being flooded
+			delay(delayMS);
+			time += delayMS;
+		}
+	}
+	
+	//stop motor once limit is reached
+	motorStop(device, brake);
+	
+	//inform user if timeout condition occured
+	if(time>timeout)
+	{
+		printf("movement timed out, motor isnt powered or it got stuck\n");
+		return -3;
+	}
+		
+	return 0;
+}
+
+//convert integer to a boolean array, needed so register read data is easier to manipulate
+int intToBits(bool* array, int number)
+{
+	int i;
+	
+	if(number>255)
+	{
+		for(i=7; i>=0; i--)
+		{
+			array[i] = true;
+		}
+		return -1;
+	}
+	
+	for(i=7; i>=0; i--)
+	{
+		if(number >= (pow(2,i)))
+		{
+			number = number -pow(2,i);
+			array[i] = true;
+		}
+		else
+		{
+			array[i] = false;
+		}
+		
+		
+	}
+	
+	
+	return 0;
+}
+
+//converts a boolean array back to an integer, integer format is needed to send the dad back to the sx1509
+int bitsToInt(bool array[8])
+{
+	int i;
+	int number = 0;
+	for(i=0; i<8; i++)
+	{
+		if(array[i])
+		{
+			number = number + pow(2,i);
+		}	
+	}
+	
+	return number;
+}
+
+//only added so i dont have to write out the whole command on every call
+void writeReg(int device, int memRegister, int data)
+{
+	wiringPiI2CWriteReg8 (device, memRegister, data);
+
+}
+//only added so i dont have to write out the whole command on every call
+int readReg(int device, int memRegister)
+{
+	return wiringPiI2CReadReg8 (device, memRegister);
+
+}
+
---
+++ drivers/sx1509.h
@@ -0,0 +1,178 @@
+//header file for sx1509 driver
+//created by Nicholas Mykuylowycz
+//created on April 23, 2014
+
+
+
+int initializeDevice();
+/*
+	intilization function, must be called before any other sx1509 related command
+
+		returns the device address if initialization completed successfully
+		device address must be used in all sx1509 related commands
+
+		returns -1 if an I2C connection could not be established\
+*/
+
+
+int LEDBreath(int device, int pin, int onIntensity, int offIntensity);
+/*
+	function causes LEDs to fade in and out slowly
+
+		returns 0 if function completed successfully
+
+		returns -1 and terminates function if an non fading pin address is input
+
+	device designates the address of the sx1509
+	the address is the output of the initialization function
+
+	pin designates the location of the LED you which to address
+	fade functionality is only avalible on pins 4,5,6,7 and pins 12,13,14,15 of sx1509
+	at time of writing the pins are connected as follows:
+
+		pin 4 - LED red0
+		pin 5 - LED green0
+		pin 6 - LED blue0
+		pin 7 -NC
+
+		pin 12 - LED red1
+		pin 13 - LED green1
+		pin 14 - LED blue1
+		pin 15 - NC
+
+	onIntensity sets the max brightness of the LED,
+	it is a value between 0 and 255
+		255 being the brightest value
+
+offIntensity functionality has not been implemented yet
+*/
+
+
+int LEDColor(int device, int LEDNum, int red, int green, int blue);
+/*
+	function allows the static color of each LED strip to be set
+
+		retuns 0 if function completes successfully
+
+		returns -1 if a LED strip other than 0 or 1 is input and terminates the function
+
+	device designates the address of the sx1509
+	the address is the output of the initialization function
+
+	LEDNum designates the led strip to be addressed
+	currently only LED strip 0 and LED strip 1 are connected (0 or 1)
+
+	red,green, and blue allow the intensity of each individual color to be set
+	they can be any value between 0 and 255
+		255 being the brightest value
+		for example if red=255,green=0,blue=0 the LED strip wold glow bright red
+*/
+
+
+bool readWLimit(int device, int switchNum);
+/*
+	function reads the value of the limit switches
+	
+		returns true if the limit switch has been triggered
+		
+		returns false of the limit switch HAS NOT been triggered
+		also returns false if limit switch does not exist
+	
+	device designates the address of the sx1509
+	the address is the output of the initialization function
+	
+	switchNum is the number of th switch you wish to check
+		0 is the negative limit switch
+		
+		1 is the positive limit switch
+		
+		if any other number is input the function will return false
+		and tell you the switch is not connected
+
+*/
+
+int moveToLimit(int device, bool dir, int speed, bool brake);
+/*
+	function moves W axis to positive or negative limit
+	the execuation of the function is blocking
+
+		returns 0 if function completes successfully
+
+		returns -1 if user tells the driver to move to positive limit
+		but the w axis is already at the positive limit
+
+		returns -2 if the user tells the drive to move to the negative limit
+		but the w axis is already at the negative limit
+
+		returns -3 if function times out before limit switch is reached
+
+	device designates the address of the sx1509
+	the address is the output of the initialization function
+
+	dir designates the intended direction of movement
+		true = postive direction
+		false = negative direction
+
+	speed designations the velocity to execute the movement at
+	any value between 0 and 255 can be used
+		255 is the higest speed
+		i have found using any value below 50 seems to stall the motor
+
+	the value of brake determines whether the motor will coast to a stop
+	or it will electronically brake
+		true = electronic brake
+		false = coast to stop
+*/
+
+
+int motorMove(int device, bool dir, int speed);
+/*
+	function moves motor in a given direction
+	(this is a "dumb" movement which will not hault if a limit switch is depressed
+	
+		returns 0 in all situations
+	
+	device designates the address of the sx1509
+	the address is the output of the initialization function
+	
+	dir designates the intended direction of movement
+		true = postive direction
+		false = negative direction
+		
+	speed designations the velocity to execute the movement at
+	any value between 0 and 255 can be used
+		255 is the higest speed
+		i have found using any value below 50 seems to stall the motor
+*/
+
+int motorStop(int device, bool brake);
+/*
+	function which haults motor movement
+	
+		returns 0 in all situations	
+	
+	device designates the address of the sx1509
+	the address is the output of the initialization function
+	
+	the value of brake determines whether the motor will coast to a stop
+	or it will electronically brake
+		true = electronic brake
+		false = coast to stop 
+*/
+
+void rave(int device, int cycles, int delayMS)
+/*
+	this is a blocking function which randomly changes the colors of the LED strips
+	this function contains code which is vital to the proper operation of the program
+	you should not attempt to remove it from the code
+	dont even try to understand the very very complicated code behind its operation
+	any attempt disect/remove this code could result in irreversible brain damage
+	
+	device designates the address of the sx1509
+	the address is the output of the initialization function
+	
+	cycles designates how many color change loops to perform
+	
+	delayMS designates the approximate millisecond delay between color change events
+*/
+
