44ed7482541a80b9b6e151ff8b58d5c754b2a500
nickmyk <nanosecrepair@gmail.com>
2014-06-22 07:02:26 +0000

    driver can now read and write to memory registers, potentiometer works as intended, code will need to be cleaned up before integrating with rest of program

--- drivers/mcp4451.c
+++ drivers/mcp4451.c
@@ -11,17 +11,18 @@
 #include "i2c_helper.c"		//helper functions and definitions for i2c
 
 /*	following includes need to be in parent file	*/ 
-//include "../y.PIC.h" // DEBUG()
-//include "../slave/commands.h"
-//include <wiringPi>
-//include <wiringPiI2C>
+#include "../y.PIC.h" // DEBUG()
+#include "../slave/commands.h"
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+#include "mcp4451.h"
 
 //int intToBits(bool * array, int number);	//moved to i2c helper
 //int bitsToInt(bool array[8]);	
 
 //mcp4451 internal register address
-const int MCPADDR_READ	= 0x59;		//address' assume A0 and A1 are set to 0
-const int MCPADDR_WRITE	= 0x58;
+const int MCPADDR	= 0x2f;		//address' assume A0 and A1 are set to 1
 const int TCON0		= 0x04;
 const int TCON1		= 0x0A;
 const int POT0		= 0x00;
@@ -29,22 +30,31 @@ const int POT1		= 0x01;
 const int POT2		= 0x06;
 const int POT3		= 0x07;
 
-#ifdef MCP_DRIVER_MAIN
+//#ifdef MCP_DRIVER_MAIN
 int main (void)
 {
 	//create identifier for digital potentiometer
 	int mcp;
+	int tmp;
 	
-	mcp = intializeMCP();
+	mcp = intitializeMCP();
+	printf("initialized to addr: %d\n", mcp);
+	
+	setPotValue(mcp, POT3, (int)127);
+	
+	tmp = readPotValue(mcp, POT3);
+	printf("pot3 value set to: %d\n", tmp);
+	
+	
 
 }
-#endif	/* MCP_DRIVER_MAIN */
+//#endif	/* MCP_DRIVER_MAIN */
 
 int intitializeMCP()
 {
 	int device;
 	
-	if((device = wiringPiI2CSetup(MCPADDR_READ)) == -1)
+	if((device = wiringPiI2CSetup(MCPADDR)) == -1)
 	{
 		// error out, caller must handle errors:
 		return -1;
@@ -63,20 +73,20 @@ void setPotValue(int device, int pot_addr, int value)
 	intToBits(addr_bits, pot_addr);
 	
 	//rearrage address
-	addr_bits[0] = addr_bits[4];
-	addr_bits[1] = addr_bits[5];
-	addr_bits[2] = addr_bits[6];
-	addr_bits[3] = addr_bits[7];
+	addr_bits[7] = addr_bits[3];
+	addr_bits[6] = addr_bits[2];
+	addr_bits[5] = addr_bits[1];
+	addr_bits[4] = addr_bits[0];
 	
 	//add the write command bits
-	addr_bits[4] = 0;
-	addr_bits[5] = 0;
+	addr_bits[3] = 0;
+	addr_bits[2] = 0;
 	
 	//dont care about this bit, setting to 0
-	addr_bits[6] = 0;
+	addr_bits[1] = 0;
 	
 	//this is technically a data bit but its out of our working range
-	addr_bits[7] = 0;
+	addr_bits[0] = 0;
 	
 	//convert bits back to integer
 	pot_addr = bitsToInt(addr_bits);
@@ -99,20 +109,20 @@ int readPotValue(int device, int pot_addr)
 	intToBits(addr_bits, pot_addr);
 	
 	//rearrage address
-	addr_bits[0] = addr_bits[4];
-	addr_bits[1] = addr_bits[5];
-	addr_bits[2] = addr_bits[6];
-	addr_bits[3] = addr_bits[7];
+	addr_bits[7] = addr_bits[3];
+	addr_bits[6] = addr_bits[2];
+	addr_bits[5] = addr_bits[1];
+	addr_bits[4] = addr_bits[0];
 	
 	//add the write command bits
-	addr_bits[4] = 1;
-	addr_bits[5] = 1;
+	addr_bits[3] = 1;
+	addr_bits[2] = 1;
 	
 	//dont care about this bit, setting to 0
-	addr_bits[6] = 0;
+	addr_bits[1] = 0;
 	
 	//this is technically a data bit but its out of our working range
-	addr_bits[7] = 0;
+	addr_bits[0] = 0;
 	
 	//convert bits back to integer
 	pot_addr = bitsToInt(addr_bits);
