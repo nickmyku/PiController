5918e396e80f20aa5f21fc86ecde4be34771f5c3
shjmy <joseph.shim@outlook.com>
2014-10-07 19:45:39 -0400

    Added a super basic header for mlx90614.c
    Improve and document better after test

---
+++ drivers/mlx90614.h
@@ -0,0 +1,12 @@
+/*
+Embark Industries
+Raspberry Pi I2C Interface for MLX90614
+IR Temp Sensor
+Created By: Joseph Yosup Shim
+Creation Date: 10/07/14
+H FILE
+*/
+#ifndef MLX90614_H_
+int initializeMLX(void);
+
+#endif
